c0c5dd1faa43adedbf48869b0571b2fb8f4aab94
taeuk-kang <taeuk_kang@naver.com>
2019-07-20 00:17:36 +0900

    🆙  Http -> Https 프로토콜 변경(인증서up) (#17)
    - Http -> Https 프로토콜 변경(인증서up) (#17)

--- server/cors-server.js
+++ server/cors-server.js
@@ -1,11 +1,17 @@
-var host = process.env.HOST || `0.0.0.0`
-var port = process.env.PORT || 8080
+const fs = require('fs')
+const host = process.env.HOST || `0.0.0.0`
+const port = process.env.PORT || 8080
 
-var cors_proxy = require(`cors-anywhere`)
+const cors_proxy = require(`cors-anywhere`)
+
 cors_proxy.createServer({
     originWhitelist: [], 
     requireHeader: [],
-    removeHeaders: [`cookie`, `cookie2`]
+    removeHeaders: [`cookie`, `cookie2`],
+    httpsOptions: {
+        key: fs.readFileSync(`/root/private.key`),
+        cert: fs.readFileSync(`/root/certificate.crt`),
+    },
 }).listen(port, host, function() {
     console.info(`Running cors`)
 })
