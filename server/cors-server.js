df74e8c038fd976d5850ff61c5884d0124c3ab8d
taeuk-gang <gmdkrl@hanyang.ac.kr>
2019-07-26 23:05:27 +0900

    :bug: 환경변수 변경에 따른, CORS서버 설정 변경

--- server/cors-server.js
+++ server/cors-server.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
-const host = process.env.HOST || `0.0.0.0`
-const port = process.env.PORT || 8080
+const host = `0.0.0.0`
+const port = 8080
 
 const cors_proxy = require(`cors-anywhere`)
 
@@ -12,7 +12,6 @@ cors_proxy.createServer({
         key: fs.readFileSync(`/root/private.key`),
         cert: fs.readFileSync(`/root/certificate.crt`),
     },
-    setHeaders: {"User-Agent": `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Whale/1.5.73.16 Safari/537.36`},
 }).listen(port, host, function() {
     console.info(`Running cors`)
 })
