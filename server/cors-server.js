59d29165f7a0e6a2bf5eb69dcefd2dcba0b80c37
taeuk-gang <gmdkrl@hanyang.ac.kr>
2019-07-27 04:17:47 +0900

    :bug: user-agent 문제로 모바일 환경에서 안되던 버그픽스

--- server/cors-server.js
+++ server/cors-server.js
@@ -6,6 +6,9 @@ const cors_proxy = require(`cors-anywhere`)
 
 cors_proxy.createServer({
     originWhitelist: [], 
+    setHeaders: {
+        'user-agent': `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Whale/1.5.73.16 Safari/537.36`,
+    },
     requireHeader: [],
     removeHeaders: [`cookie`, `cookie2`],
     httpsOptions: {
