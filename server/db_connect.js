749d509823dc5f3816ce42410624c47d969ceea5
Jeon Ga Bin <hyanghope@naver.com>
2019-07-31 00:03:18 +0900

    api 구현 (#51)
    * insert_data 구현
    
    * config 수정
    
    * env 수정
    
    * #41 swagger 작성
    
    * #47 table 내의 link 갯수 구하는 함수 구현
    
    * #47 count갯수가 많거나 없는 예외처리 구현
    
    * #47 API RESPONSE SPEC에 맞춰 구현

--- server/db_connect.js
+++ server/db_connect.js
@@ -5,11 +5,11 @@ require('dotenv').config();
 
 let dbconnect = () => { 
     let connection =  mysql.createConnection({
-        host: process.env.HOST,
-        port: process.env.PORT,
-        user: process.env.USER,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE
+        host: "106.10.53.225",
+        port: 3306,
+        user: "root",
+        password: "dnrxo`1`1",
+        database: "d2"
     });
     return connection
 }
