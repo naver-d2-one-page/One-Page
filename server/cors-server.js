41664f003b9bd34fa0227c25dca256160cf0b531
taeuk-gang <gmdkrl@hanyang.ac.kr>
2019-07-31 20:50:27 +0900

    fix/conflict

--- server/cors-server.js
+++ server/cors-server.js
@@ -1,22 +1,14 @@
 const fs = require('fs')
-<<<<<<< HEAD
 const host = `0.0.0.0`
 const port = 8080
-=======
-const host = process.env.HOST || `0.0.0.0`
-const port = process.env.PORT || 8080
->>>>>>> dfca6dc68710be43c4e70856e25b82ceea6d7ca8
 
 const cors_proxy = require(`cors-anywhere`)
 
 cors_proxy.createServer({
     originWhitelist: [], 
-<<<<<<< HEAD
     setHeaders: {
         'user-agent': `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Whale/1.5.73.16 Safari/537.36`,
     },
-=======
->>>>>>> dfca6dc68710be43c4e70856e25b82ceea6d7ca8
     requireHeader: [],
     removeHeaders: [`cookie`, `cookie2`],
     httpsOptions: {
